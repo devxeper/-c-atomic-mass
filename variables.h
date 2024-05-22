b92c21012af9b9555aaefb878f6575d4f0bb2760
untitled <109837842+untitled29x@users.noreply.github.com>
2024-05-21 22:21:57 -0400

    Create variables.h

---
+++ variables.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+
+ // default variables
+
+    int element;
+    double volume;     
+    double pressure;
+    double temperature;
+    double density;
+    
+    std::string useraction;
